6b210a140918faa3dfc47e3c8eb875f9b4cb052b
90houlaoheshang <907333918@qq.com>
2020-08-21 18:36:21 +0800

    更换库名

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="HuobiAsyncWs",
+    name="HuobiAsyncWebsocket",
     version="1.0.0",
     author="Antas",
     author_email="",
