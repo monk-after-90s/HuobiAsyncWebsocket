1881e328f5f900a6db4478c62238e2275bf4ce80
90houlaoheshang <907333918@qq.com>
2020-08-21 17:37:21 +0800

    更新ws连接时，删除老的ws更新任务，以通过回调关闭之前开的ws连接

--- HuobiAsyncWebsocket/HuobiAsyncWebsocket.py
+++ HuobiAsyncWebsocket/HuobiAsyncWebsocket.py
@@ -4,11 +4,11 @@ import traceback
 from copy import deepcopy
 
 import beeprint
+from ensureTaskCanceled import ensureTaskCanceled
 from loguru import logger
 
 import websockets
 from NoLossAsyncGenerator import NoLossAsyncGenerator
-from ensureTaskCanceled import ensureTaskCanceled
 
 from HuobiAsyncWebsocket.UrlParamsBuilder import create_signature, UrlParamsBuilder
 
@@ -26,7 +26,6 @@ class HuobiAsyncWs:
         self._handlers = set()
         self._exiting = False
         self._update_ws_task: asyncio.Task = None
-        self._ws_generator: NoLossAsyncGenerator = None
         self._update_ws_event = asyncio.Event()  # 用来提示所有的需要更新连接的情况
         self._update_ws_event.set()  # 刚开始就需要更新连接，最初新建
 
@@ -70,6 +69,7 @@ class HuobiAsyncWs:
                 # 等心跳只能等30s，否则超时
                 ping = await asyncio.wait_for(ping_aiter.__anext__(), 30)
             except asyncio.TimeoutError:  # 等心跳超时 todo 测试故意心跳超时
+                logger.debug('Ping timeout.')
                 self._update_ws_event.set()
             else:
                 pong = json.dumps({
@@ -109,42 +109,35 @@ class HuobiAsyncWs:
         }
         await self._ws.send(json.dumps(auth_request))
 
-    async def _update_ws(self, old_ws):
+    async def _update_ws(self, old_ws_update_ws_task):
         '''
         安全处理老的ws连接，建立新的ws连接
 
-        :param old_ws: 老的ws连接
+        :param old_ws_update_ws_task: 老的ws连接
         :return:
         '''
-        # 安全退出老的ws
-        if isinstance(self._ws_generator, NoLossAsyncGenerator):
-            await self._ws_generator.close()
-        if isinstance(old_ws, asyncio.Task) and not old_ws.done():
-            asyncio.create_task(ensureTaskCanceled(old_ws))
 
         self._ws = await websockets.connect(self.ws_baseurl)
-        logger.info('New huobi ws connection opened.')
+        self._update_ws_task._ws_opened = self._ws
+
+        logger.debug('New huobi ws connection opened.')
         # 鉴权
         asyncio.create_task(self._get_authentication())
 
-        # 如果被删除，负责关闭自己打开的ws连接
-        def cancel_ws(task: asyncio.Task):
-            async def close_old_ws(ws):
-                await asyncio.create_task(ws.close())
-                logger.info('Old huobi ws connection closed.')
+        if isinstance(old_ws_update_ws_task, asyncio.Task) and not old_ws_update_ws_task.done():
+            asyncio.create_task(ensureTaskCanceled(old_ws_update_ws_task))
+            # logger.debug('Start canceling old_ws_update_ws_task')
+            # logger.debug('old_ws_update_ws_task .')
 
-            asyncio.create_task(close_old_ws(task._opened_ws))
-
-        self._update_ws_task._opened_ws = self._ws
-        self._update_ws_task.add_done_callback(cancel_ws)
         # 通知实例化完成
         if not self._ws_ok.done():
             self._ws_ok.set_result(None)
-        self._ws_generator = NoLossAsyncGenerator(self._ws)
-        async for msg in self._ws_generator:
+        async for msg in self._ws:
             try:
+                # logger.debug(repr(old_ws_update_ws_task))
                 msg = json.loads(msg)
                 logger.debug('\n' + beeprint.pp(msg, output=False, string_break_enable=False, sort_keys=False))
+                # logger.debug('\n' + repr(self._update_ws_task))
                 tasks = []
                 for handler in self._handlers:
                     if asyncio.iscoroutinefunction(handler):
@@ -161,6 +154,7 @@ class HuobiAsyncWs:
                         pass
             except:
                 logger.error('\n' + traceback.format_exc())
+        logger.debug('Data stream exhausted.')
 
     async def _ws_manager(self):
         # 心跳处理
@@ -171,6 +165,16 @@ class HuobiAsyncWs:
             # 更新连接
             self._update_ws_task = asyncio.create_task(self._update_ws(self._update_ws_task))
             self._update_ws_event.clear()
+
+            # 若被删除，则需要安全退出其打开的ws连接
+            def _update_ws_task_canceled_then_exit_ws(task: asyncio.Task):
+                async def close_ws(ws: websockets.WebSocketClientProtocol):
+                    await ws.close()
+                    logger.debug('Old _update_ws_task safely closed.')
+
+                asyncio.create_task(close_ws(task._ws_opened))
+
+            self._update_ws_task.add_done_callback(_update_ws_task_canceled_then_exit_ws)
 
     @classmethod
     async def create_instance(cls, apikey, secret):
